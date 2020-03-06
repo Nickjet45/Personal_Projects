1cf622cb12584e228c49eef5475039d461c88093
Nickjet45 <60004491+Nickjet45@users.noreply.github.com>
2020-03-06 10:02:41 -0600

    Update Amazon.py

--- Amazon.py
+++ Amazon.py
@@ -10,18 +10,26 @@ from bs4 import BeautifulSoup
 from Email_Bot import Mail_Bot
 
 URL = input('Please enter the URL of the item you wish to track: ')
+
 try:
-    price_track = float(input('What price would you like to be emailed at? '))
+    price_track = float(input('What price would you like to be notified at? '))
+
 except ValueError:
     print('Please enter a number')
     price_track = float(input('What price would you like to be emailed at?'))
-email_given = input('Please enter your email: ')
+
+email_response = input('Would you like to be emailed if the price falls below your target? [y/n]')
+
+if email_response is 'y':
+    email_given = input('Please enter your email: ')
+else:
+    email_given = None
 
 # Enter User-Agent by Googling "What is my User Agent"
+
 headers = {"User-Agent":
                'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit'
                '/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36'}
-
 
 # Function to check the price of an Amazon project by:
 # Gathering the elements from the page
@@ -44,7 +52,6 @@ def check_Price(price_to_fall, email):
     # If the first id doesn't exist, then it means the item is on sale
     # So it begins to check for that second id
     if 'amazon' in URL:
-
         try:
             price = soup.find(id="priceblock_ourprice").get_text()
         except AttributeError:
@@ -52,6 +59,7 @@ def check_Price(price_to_fall, email):
 
     # Converts the Price from a string to a float value so that it can be used to
     # compare data
+
     converted_price = float(price[1:4])
 
     # When the price of the product is below or at the price you wish to buy the product at:
@@ -62,21 +70,24 @@ def check_Price(price_to_fall, email):
     # If the price didn't fall below the desired price:
     # The program sleeps for 43200 seconds, approximately half a day and then checks again
     # Repeating the process until the price has fallen
+
     while converted_price <= price_to_fall:
 
         if converted_price <= price_to_fall:
-            Mail_Bot(email, title.strip() + ' Price has fallen!',
-                     'The price of {} has fallen to a price of {} '
-                     '\n Your target price was originally: ${} \n ' \
-                     'Buy now at: \n {}'
-                     .format(title.strip(), price, price_track, URL))
-
-            break
+            if email_response is 'y':
+                Mail_Bot(email, title.strip() + ' Price has fallen!',
+                         'The price of {} has fallen to a price of {} '
+                         '\n Your target price was originally: ${} \n ' \
+                         'Buy now at: \n {}'
+                         .format(title.strip(), price, price_track, URL))
+            elif email_response is 'n':
+                print('The current price is ${}'.format(converted_price))
+        break
 
     while converted_price > price_to_fall:
         print('Price has not fallen below ${}, '
               'will check again tomorrow. '
-              'Price is currently {}'.format(price_to_fall, price))
+              'Price is currently ${}'.format(price_to_fall, price))
         time.sleep(43200)
         check_Price(price_track, email)
 
